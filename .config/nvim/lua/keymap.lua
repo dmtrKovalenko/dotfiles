db934d3d6a0232c5479293fd0955028fe8cc4472
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2025-10-22 12:00:07 -0700

    Wed Oct 22 19:00:05 UTC 2025

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -184,22 +184,23 @@ vim.keymap.set("n", "<C-D-h>", "<cmd>wincmd h<CR>", { silent = true, nowait = tr
 vim.keymap.set("n", "<C-D-l>", "<cmd>wincmd l<CR>", { silent = true, nowait = true, desc = "Go to right window" })
 vim.keymap.set("n", "<C-D-j>", "<cmd>wincmd j<CR>", { silent = true, nowait = true, desc = "Go to bottom window" })
 vim.keymap.set("n", "<C-D-k>", "<cmd>wincmd k<CR>", { silent = true, nowait = true, desc = "Go to top window" })
-vim.keymap.set(
-  "n",
-  "<C-D-s>",
-  "<cmd>rightbelow vsplit<cr>",
-  { silent = true, nowait = true, desc = "Split window vertically" }
-)
 
 vim.keymap.set("n", "<C-D-Up>", "<Cmd>resize +2<CR>", { silent = true, nowait = true, desc = "Taller" })
 vim.keymap.set("n", "<C-D-Down>", "<Cmd>resize -2<CR>", { silent = true, nowait = true, desc = "Shorter" })
 vim.keymap.set("n", "<C-D-Right>", "<Cmd>vertical resize +5<CR>", { silent = true, nowait = true, desc = "Wider" })
 vim.keymap.set("n", "<C-D-Left>", "<Cmd>vertical resize -5<CR>", { silent = true, nowait = true, desc = "Narrower" })
+vim.keymap.set("n", "<C-D-o>", "<cmd>wincmd o<CR>", { silent = true, nowait = true, desc = "Quit other windows" })
 
 vim.keymap.set(
   "n",
   "<C-D-v>",
+  "<cmd>rightbelow vsplit<cr>",
+  { silent = true, nowait = true, desc = "Split window vertically" }
+)
+
+vim.keymap.set(
+  "n",
+  "<C-D-s>",
   "<cmd>rightbelow split<cr>",
   { silent = true, nowait = true, desc = "Split window horizontally" }
 )
-vim.keymap.set("n", "<C-D-o>", "<cmd>wincmd o<CR>", { silent = true, nowait = true, desc = "Quit other windows" })
