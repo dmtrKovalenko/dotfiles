ee629a5ae214c12fab0de887dfc5099bf4c7205c
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2025-11-18 14:30:31 -0800

    Fix aws login for ssh

--- .config/fish/functions/awsclaude.fish
+++ .config/fish/functions/awsclaude.fish
@@ -3,7 +3,12 @@ function awsclaude
     set -x AWS_REGION "us-west-2"
     set -x CLAUDE_CODE_USE_BEDROCK 1
 
-    aws sso login --profile dev
+    if not set -q SSH_CONNECTION
+        aws sso login --profile dev
+    else
+        # have to open the browser manually over local shell
+        aws sso login --profile dev --no-browser --use-device-code
+    end
     eval "$(aws configure export-credentials --profile dev --format env)" && \
     claude --model us.anthropic.claude-sonnet-4-5-20250929-v1:0
 end
