90108ddcef33aa3314712c22742f0b825b4a9bba
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2025-11-19 16:37:25 -0800

    Thu Nov 20 00:37:25 UTC 2025

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -4,5 +4,5 @@ function fish_greeting
     echo ""
     echo -n "It's "; set_color -o cyan; echo -n (date +%A); set_color normal; echo -n ". The time is "; set_color -o magenta; echo -n (date +%H:%M ); set_color normal;
     echo ""
-    echo You are on $hostname
+    echo -n "You are using "; set_color -o bryellow; echo -n $hostname; set_color normal;  echo " btw"; 
 end
