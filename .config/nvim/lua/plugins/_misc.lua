b3f8523f080c866765a43184ed3714d884951e89
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2026-03-26 12:00:01 -0700

    Thu Mar 26 19:00:01 UTC 2026

--- .config/nvim/lua/plugins/_misc.lua
+++ .config/nvim/lua/plugins/_misc.lua
@@ -29,7 +29,8 @@ return {
   },
   {
     "dmtrkovalenko/fff.nvim",
-    -- dir = "~/dev/fff.nvim",
+    dir = "~/dev/fff.nvim",
+    -- branch = "feat/prebuild",
     build = function()
       -- No more need to cargo build!!!!
       require("fff.download").download_binary()
